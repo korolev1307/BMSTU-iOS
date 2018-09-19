dceb11322ab930ac10655acf4366bf0044972d5a
korolev1307 <korolev1307@yandex.ru>
2018-09-19 22:39:58 +0300

    Upload HW#1

---
+++ HW1/HW1/main.swift
@@ -0,0 +1,133 @@
+//
+//  main.swift
+//  HW1
+//
+//  Created by Александр Королев on 16.09.2018.
+//  Copyright © 2018 Korolev. All rights reserved.
+//
+
+import Foundation
+protocol PrintAll {
+   func PrintPart()
+}
+
+enum currentValue: String {
+    case RUR = "RUR"
+    case USD = "USD"
+    case EUR = "EUR"
+}
+
+class MainData {
+    let id: Int
+    var balance: Double
+    let currency: currentValue
+    
+    init(id: Int, balance: Double, currency: String ) {
+        self.id = id
+        self.balance = balance
+        self.currency = currentValue(rawValue: currency)! //Why I need to use ! in the end? (I used it because swift had errors)
+    }
+    
+    func PrintMain() {
+        print("MainData: \n id: \(id), balance: \(balance), currency: \(currency)\n")
+    }
+}
+
+class Account: MainData, PrintAll {
+    let Description: String
+    var Offer: String?
+    
+    init(id: Int, balance: Double, currency: String, Description: String, Offer: String? = nil) {
+        self.Description = Description
+        if let Offer = Offer {
+            self.Offer = Offer
+        }
+        super.init(id: id, balance: balance, currency: currency) //Why it's not working before Description and Offer init?
+    }
+    
+    func PrintPart() {
+        super.PrintMain()
+        print("Account:\n Description:\(Description) Offer:\(Offer ?? "Not Stated")\n\n")
+    }
+}
+
+class ReissueInfo: PrintAll {
+    let Date: Date
+    let Info: String
+    let Address: String
+    
+    init(Date: Date, Info: String, Address: String = "Main Office")
+    {
+        self.Date = Date
+        self.Info = Info
+        self.Address = Address
+    }
+    
+    func PrintPart() {
+        let GoodDate = DateFormatter()
+        GoodDate.dateFormat = "dd --- MM --- YYYY"
+        print("ReissueInfo:\n Date: \(GoodDate.string(from: Date)) Info: \(Info) Address: \(Address)\n\n")
+    }
+}
+
+class Card: MainData, PrintAll {
+    var ReissueInfo: ReissueInfo?
+    
+    init(id: Int, balance: Double, currency: String, ReissueInfo: ReissueInfo? = nil) {
+        if let ReissueInfo = ReissueInfo {
+            self.ReissueInfo = ReissueInfo
+        }
+        super.init(id: id, balance: balance, currency: currency)
+    }
+    
+    func PrintPart() {
+        super.PrintMain()
+        if let ReissueInfo = ReissueInfo {
+            ReissueInfo.PrintPart()
+        }
+    }
+}
+
+class ServerResponce: PrintAll {
+    var Cards: [Card]
+    var Accounts: [Account]
+    
+    init(Cards: [Card], Accounts: [Account]) {
+        self.Cards = Cards
+        self.Accounts = Accounts
+    }
+    
+    func PrintPart() {
+        for Card in Cards {
+            Card.PrintPart()
+        }
+        for Account in Accounts {
+            Account.PrintPart()
+        }
+    }
+}
+
+let ac1 = Account(id: 1, balance: 1000, currency: "RUR", Description: "lol kek cheburek") // Offer = nil
+let ac2 = Account(id: 2, balance: 3228, currency: "USD", Description: "kek lol cheburek", Offer: "coupon PAPAZ" ) // Offer != nil
+let accs = [ac1, ac2]
+let currentDate = "19 --- 09 --- 2018"
+var DateFormat = DateFormatter()
+DateFormat.dateFormat = "dd --- MM --- YYYY"
+var Date = DateFormat.date(from: currentDate)
+let reissueInfo = ReissueInfo(Date: Date!, Info: "i'm so tired", Address: "PushkinoGalushkino") // I'm used it againg
+let card1 = Card(id: 3, balance: 322, currency: "EUR", ReissueInfo: reissueInfo) //reissueInfo != nil
+let card2 = Card(id: 4, balance: 228, currency: "RUR") //reissueInfo = nil
+let cards = [card1, card2]
+let response = ServerResponce(Cards: cards, Accounts: accs)
+
+var array: [PrintAll] = []
+
+array.append(contentsOf: cards)
+
+array.append(contentsOf: accs)
+
+array.append(response)
+
+for item in array {
+    item.PrintPart()
+}
